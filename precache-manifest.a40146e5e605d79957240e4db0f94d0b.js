58e147bb77e00ac88a8f6f0057a878cc12bdd658
elantry <mohamedahmed25173@gmail.com>
2022-02-16 16:01:37 +0200

    Updates

---
+++ precache-manifest.a40146e5e605d79957240e4db0f94d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057da4513aaab7ea5300f8dac76b460b",
+    "url": "/react-app1/index.html"
+  },
+  {
+    "revision": "4e163a52c04ea94994f8",
+    "url": "/react-app1/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/react-app1/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app1/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e163a52c04ea94994f8",
+    "url": "/react-app1/static/js/main.fdfdfbaa.chunk.js"
+  },
+  {
+    "revision": "14a6410f341fdc27de77",
+    "url": "/react-app1/static/js/runtime-main.0e80472d.js"
+  }
+]);
